061ccbfd060e4578e8dabae96679588de6d1a6d1
Matvienko Oleg <OMatvienko@ics.perm.ru>
2021-03-05 14:11:59 +0500

    Remove unnecessary usings

--- src/SuperSimpleContactList/Tests/DataObjectFacts.Regenerated.cs
+++ src/SuperSimpleContactList/Tests/DataObjectFacts.Regenerated.cs
@@ -14,8 +14,6 @@ namespace NewPlatform.SuperSimpleContactList
     using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
-    using System.Text;
-    using System.Threading.Tasks;
     using Xunit;
 
     public partial class DataObjectFacts
