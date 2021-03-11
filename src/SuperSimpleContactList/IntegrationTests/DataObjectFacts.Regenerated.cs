475319799e05d2a77163bf20664775de35663c60
Matvienko Oleg <OMatvienko@ics.perm.ru>
2021-03-11 20:31:26 +0500

    Add integration tests

---
+++ src/SuperSimpleContactList/IntegrationTests/DataObjectFacts.Regenerated.cs
@@ -0,0 +1,21 @@
+﻿namespace NewPlatform.SuperSimpleContactList
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public partial class DataObjectFacts
+    {
+        /// <summary>
+        /// Получить классы объектов данных,
+        /// которые не требуется вычитывать из БД.
+        /// </summary>
+        /// <returns>Список классов, которые не требуется вычитывать из БД.</returns>
+        private partial IEnumerable<Type> GetNotSelectableTypes()
+        {
+            return Enumerable.Empty<Type>();
+        }
+    }
+}
---
+++ src/SuperSimpleContactList/IntegrationTests/Startup.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Пришлось сменить неймспейс, потому что иначе XUnit.DependencyInjection версии 7.х не может подтянуть startup-класс.
+ * В версии 7.х в отличие от предыдущих версий этим поведением нельзя управлять.
+ */
+namespace SuperSimpleContactList.IntegrationTests
+{
+
+    using ICSSoft.STORMNET.Business;
+    using ICSSoft.STORMNET.Security;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+
+    public class Startup
+    {
+        public void ConfigureHost(IHostBuilder hostBuilder) =>
+            hostBuilder
+                .ConfigureHostConfiguration(builder => { builder.AddJsonFile("appsettings.json"); })
+                .ConfigureAppConfiguration((context, builder) => { });
+
+        public void ConfigureServices(IServiceCollection services, HostBuilderContext context)
+        {
+            string connStr =context.Configuration["DefConnStr"];
+            services.AddSingleton<ISecurityManager, EmptySecurityManager>();
+            services.AddSingleton<IDataService, PostgresDataService>(f => new PostgresDataService(f.GetService<ISecurityManager>()) { CustomizationString = connStr });
+
+        }
+    }
+}
