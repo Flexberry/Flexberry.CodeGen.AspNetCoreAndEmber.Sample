90b81af4423616f42ce970d18df56b2aceb7ae4c
AntonIV8 <anton-iv87@yandex.ru>
2023-07-27 09:57:46 +0500

    Add docker for web api

--- src/Docker/create-image.cmd
+++ src/Docker/create-image.cmd
@@ -1,3 +1,5 @@
 docker build --no-cache -f SQL\Dockerfile.PostgreSql -t supersimplecontactlist/postgre-sql ../SQL
 
 docker build --no-cache -f Dockerfile -t supersimplecontactlist/app ../..
+
+docker build --no-cache -f Dockerfile.WebApi -t supersimplecontactlist/web-api ../..
