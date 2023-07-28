838e8b55b8e5e1464a93327c73466b4e28f99e0d
TeterinaSvetlana <40786685+TeterinaSvetlana@users.noreply.github.com>
2023-07-28 17:15:13 +0500

    code review

--- src/Docker/create-image.cmd
+++ src/Docker/create-image.cmd
@@ -6,4 +6,4 @@ docker build --no-cache -f Dockerfile.WebApi -t supersimplecontactlist/web-api .
 
 docker build --no-cache -f Dockerfile.BackgroundService -t supersimplecontactlist/background-service ../..
 
-docker build --no-cache -f Dockerfile.ConsoleApp 
+docker build --no-cache -f Dockerfile.ConsoleApp -t supersimplecontactlist/console-app ../..
