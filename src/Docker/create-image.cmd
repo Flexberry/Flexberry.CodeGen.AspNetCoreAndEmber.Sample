15fac280ace8bb760ab100ea7e8acdd8db359d37
Totoro59russ <97888319+Totoro59russ@users.noreply.github.com>
2023-08-01 12:26:58 +0500

    fix PR

--- src/Docker/create-image.cmd
+++ src/Docker/create-image.cmd
@@ -4,3 +4,5 @@ docker build --no-cache -f Dockerfile.WebApi -t supersimplecontactlist/web-api .
 docker build --no-cache -f Dockerfile.BackgroundService -t supersimplecontactlist/background-service ../..
 
 docker build --no-cache -f Dockerfile.ConsoleApp -t supersimplecontactlist/console-app ../..
+
+docker build --no-cache -f SQL\Dockerfile.PostgreSql -t supersimplecontactlist/postgre-sql ../SQL
--- src/Docker/push-image.cmd
+++ src/Docker/push-image.cmd
@@ -1,6 +1,8 @@
 #!/bin/sh
-docker push supersimplecontactlist/postgre-sql
+docker push supersimplecontactlist/web-api 
 
-docker push supersimplecontactlist/app
+docker push supersimplecontactlist/background-service 
 
-docker push supersimplecontactlist/console-app
+docker push supersimplecontactlist/console-app 
+
+docker push supersimplecontactlist/postgre-sql 
